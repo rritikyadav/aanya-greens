9639b069f733e6a0bf0c0dbeb46d2a4b68942ef1
Ritikyadav09 <ritikyadhuvanshii@gmail.com>
2025-06-29 09:19:48 +0530

    okay

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import './form.css';
 
-function Form({style}){
-    return(
+function Form({ style }) {
+  return (
 
-        <>
-             <div className="formstart">
+    <>
+      <div className="formstart">
         <div className="formheading">
           <h1>For Booking</h1>
           <p>Wanna Book For Parties? Feel Free To Fill The Form</p>
         </div>
         <div className="forminput">
           <form method="post">
+            .formrow1
             <input type="text" name="name" placeholder="Name" required />
             <input type="email" name="email" placeholder="Email" required />
             <input type="tel" name="phone" placeholder="Phone" required />
@@ -31,7 +32,7 @@ function Form({style}){
           </form>
         </div>
       </div>
-        </>
-    )
+    </>
+  )
 }
 export default Form;
