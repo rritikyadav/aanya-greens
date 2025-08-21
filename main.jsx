e4ee481136e9ab5517174e3c0c94a224ddf45bc4
Ritikyadav09 <ritikydv0909@gmail.com>
2025-08-21 18:30:01 +0530

    ok

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
-import { createHashRouter , RouterProvider } from "react-router-dom";
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
 import App from './src/App.jsx';
 import Accomodations from './src/pages/accomodations.jsx';
 import Gallery from './src/pages/gallery.jsx';
@@ -9,7 +9,7 @@ import Contactus from './src/pages/contactus.jsx';
 import AboutUs from './src/pages/aboutus.jsx';
 import EventsActivities from './src/pages/events&activities.jsx';
 
-const router = createHashRouter([
+const router = createBrowserRouter([
 
   {path: '/', element: <App />},
   
