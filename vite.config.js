367997a9b5e444f07dbe13818d7f5167681fbf4e
Ritikyadav09 <ritikydv0909@gmail.com>
2026-02-14 16:55:31 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aanya-greens', // Set the base path for the project
+  base: '/', // Set the base path for the project
 });
